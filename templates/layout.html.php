fc2c4fe4da4bae81711c4ac02379753d5b5d2438
jenewland1999 <34895363+jenewland1999@users.noreply.github.com>
2020-01-01 12:38:31 +0000

    Added dynamic title to layout
    Signed-off-by: jenewland1999 <34895363+jenewland1999@users.noreply.github.com>

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
 
     <!-- Page Title -->
-    <title>iBuy Auctions</title>
+    <title><?= $title; ?> | iBuy Auctions</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/public/stylesheets/lib/bootstrap-4.4.1.min.css" />
