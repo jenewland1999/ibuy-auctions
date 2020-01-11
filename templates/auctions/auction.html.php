5de3bc009dc2b88599cb6de74ac8b3cd42cb994a
jenewland1999 <34895363+jenewland1999@users.noreply.github.com>
2020-01-11 19:14:24 +0000

    getCurrentBid() on auction page is now wrapped by formatCurrency
    Signed-off-by: jenewland1999 <34895363+jenewland1999@users.noreply.github.com>

--- templates/auctions/auction.html.php
+++ templates/auctions/auction.html.php
@@ -47,7 +47,7 @@
       <li class="list-group-item bg-transparent pl-0">Auction created by <a href="/auctions?user=<?= $user['user_id'] ?>"><?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name'], ENT_QUOTES, 'UTF-8'); ?></a></li>
       
       <!-- Current Bid -->
-      <li class="list-group-item bg-transparent pl-0">Current Bid: <?= htmlspecialchars(getCurrentBid(), ENT_QUOTES, 'UTF-8'); ?></li>
+      <li class="list-group-item bg-transparent pl-0">Current Bid: <?= htmlspecialchars(formatCurrency(getCurrentBid(), '£'), ENT_QUOTES, 'UTF-8'); ?></li>
       
       <!-- Time Remaining (NOTE: JS Handles the countdown magic) -->
       <li class="list-group-item bg-transparent pl-0">Time Remaining: <?= $timeRemaining->format('%m Months, %d Days, %h Hours, %i Minutes, %s Seconds') ?></li>
