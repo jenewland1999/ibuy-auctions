bad28ec0989824b201868ec8a407e0cae00357eb
jenewland1999 <34895363+jenewland1999@users.noreply.github.com>
2020-01-16 11:29:27 +0000

    Improved various pages surrounding auctions
    - The auction approve, disapprove, deletion (admin) and deletion (user) templates have been overhauled to have a more consistent mark-up and look. (Addresses #8, #9 and #13)
    - Created a css class called .ibuy_text to use on elements that print text to the screen that originated from user input (Addresses #9 and #13)
    
    Signed-off-by: jenewland1999 <34895363+jenewland1999@users.noreply.github.com>

--- templates/admin/auctions/approve.html.php
+++ templates/admin/auctions/approve.html.php
@@ -4,52 +4,52 @@
     <hr class="my-5" />
 
     <p class="lead">Are you sure you want to approve this auction?</p>
+    <p class="mb-5">Once disapproved the auction will no longer appear to all users and visitors.</p>
     
-    <ul class="list-group mb-3">
+    <ul class="list-group">
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Name</small></p>
-        <p><?= htmlspecialchars($auction['auction_name'], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Name</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars($auction['auction_name'], ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Description</small></p>
-        <p><?= htmlspecialchars($auction['auction_description'], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Description</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars($auction['auction_description'], ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Timestamp</small></p>
-        <p><?= htmlspecialchars($timestampDate . ' at ' . $timestampTime, ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Timestamp</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars(getFormattedDateTime($auction['auction_timestamp']), ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Category</small></p>
-        <p><?= htmlspecialchars($category['category_name'], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Category</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars($category['category_name'], ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Author</small></p>
-        <p><?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name'] . ' <' . $user['user_email'] . '>', ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Author</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars(getUserFullName($user) . ' <' . $user['user_email'] . '>', ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Start Date</small></p>
-        <p><?= htmlspecialchars($startDate . ' at ' . $startTime, ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Start Date</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars(getFormattedDateTime($auction['start_date']), ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction End Date</small></p>
-        <p><?= htmlspecialchars($endDate . ' at ' . $endTime, ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction End Date</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars(getFormattedDateTime($auction['end_date']), ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Start Price</small></p>
-        <p><?= htmlspecialchars($auction['start_price'], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Start Price</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars(formatCurrency($auction['start_price'], '£'), ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
       <li class="list-group-item">
-        <p class="my-0"><small>Auction Buy Price</small></p>
-        <p><?= htmlspecialchars($auction['buy_price'], ENT_QUOTES, 'UTF-8'); ?></p>
+        <p class="mb-0 text-muted"><small>Auction Buy Price</small></p>
+        <p class="mb-0 ibuy__text"><?= htmlspecialchars(formatCurrency($auction['buy_price'], '£'), ENT_QUOTES, 'UTF-8'); ?></p>
       </li>
     </ul>
     
-    <form action="" method="post">
-      <input type="hidden" name="auction_id" id="auction_id" value="<?= $_GET['id'] ?>" required />
-      <div class="form-group">
-        <button type="submit" name="submit" class="btn btn-block btn-success">Approve Auction</button>
-      </div>
+    <form action="" method="post" class="my-2">
+      <input type="hidden" name="auction_id" id="auction_id" value="<?= $auction['auction_id'] ?>" required />
+      <button type="submit" name="submit" class="btn btn-block btn-success">Approve Auction</button>
     </form>
+    
     <a href="/admin/auctions/" class="btn btn-block btn-secondary">Return to Auction List</a>
   </div>
 </div>
